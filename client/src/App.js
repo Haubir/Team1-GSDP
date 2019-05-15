7e0d9d350a27bba56280e1beaf2d078a2dcb70a2
trachpro <tu.phamminh.2207@gmail.com>
2019-05-15 23:13:44 +0700

    integrate gui

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,59 @@ class App extends Component {
   componentDidMount() {
   }
 
+  goTo(link) {
+    console.log("prop: ", this.props)
+    this.props.router.push(link)
+  }
+
   render() {
+    console.log(this.props.router.location)
     return (
       <div className="App">
-        {this.props.children}
+
+        <div className="w3-top">
+            <ul>
+              {/* <li><a href="https://runestone2019-team01.herokuapp.com/" style={{textDecoration: "none"}}><span className="glyphicon glyphicon-home"></span> Home</a></li> */}
+              <li><a onClick={() => this.goTo('/')} className={this.props.router.location.pathname === '/'? 'active': ''} style={{textDecoration: "none"}}>Streamer</a></li>
+              <li><a onClick={() => this.goTo('/camera')}  className={this.props.router.location.pathname === '/camera'? 'active': ''} style={{textDecoration: "none"}}>Client</a></li>
+              <li className="navbar-right"><a href="https://github.com/Haubir/Team1-GSDP" style={{textDecoration: "none"}}><span className="glyphicon glyphicon-book"></span> Repository</a></li>
+            </ul>
+          </div>
+
+          <div className="jumbotron text-center">
+            <div className="container">
+              <a href="/" className="lang-logo">
+                <img src="http://localhost:3000/images/lang-logo.png"/>
+              </a>
+              <h1>Live Video Stream - Client</h1>
+              <p>Welcome to the Runestone Robot application! On this page you can see the video.</p>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="alert alert-info text-center" role="alert">
+              To see our code, head to the <a href="https://github.com/Haubir/Team1-GSDP" className="alert-link">Team1-GSDP</a> repository.
+            </div>
+            <hr/>
+
+            <center>
+            <img src=""/>
+            </center>
+
+            <br/>
+
+          </div>
+          {this.props.children}
+          <div className="w3-container w3-black w3-center w3-opacity w3-padding-64">
+              <h1 className="w3-margin w3-xlarge">Team members:</h1>
+              <h1 className="w3-margin w3-xlarge">Billy Clerc, Công Thành Bùi, Haubir Mariwani, Huy Pham, Krishnamurthy Gajapathy, Tu Pham</h1>
+          </div>
+            
+          <footer className="w3-container w3-padding-64 w3-center w3-opacity">
+          <p>Original template provided by <a href="https://www.heroku.com/" target="_blank">Heroku</a> and <a href="https://www.w3schools.com/" target="_blank">w3schools.com</a></p>
+          <p>Livestream over server credit to https://github.com/alexcambose/webcam-base64-streaming.git</p>
+          </footer>
+          
       </div>
     );
   }
