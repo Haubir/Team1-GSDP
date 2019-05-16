94fbd9e8f4dd94909b6e8a034917a9011e5fe9c7
Bart-soldier <38186911+Bart-soldier@users.noreply.github.com>
2019-05-16 09:03:51 +0200

    Layout changes 1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
   }
-  
+
 
   componentDidMount() {
   }
@@ -28,19 +28,16 @@ class App extends Component {
         <div className="w3-top">
             <ul>
               {/* <li><a href="https://runestone2019-team01.herokuapp.com/" style={{textDecoration: "none"}}><span className="glyphicon glyphicon-home"></span> Home</a></li> */}
-              <li><a onClick={() => this.goTo('/')} className={this.props.router.location.pathname === '/'? 'active': ''} style={{textDecoration: "none"}}>Streamer</a></li>
-              <li><a onClick={() => this.goTo('/camera')}  className={this.props.router.location.pathname === '/camera'? 'active': ''} style={{textDecoration: "none"}}>Client</a></li>
+              <li><a onClick={() => this.goTo('/')} className={this.props.router.location.pathname === '/'? 'active': ''} style={{textDecoration: "none"}}>Client</a></li>
+              <li><a onClick={() => this.goTo('/camera')}  className={this.props.router.location.pathname === '/camera'? 'active': ''} style={{textDecoration: "none"}}>Camera</a></li>
               <li className="navbar-right"><a href="https://github.com/Haubir/Team1-GSDP" style={{textDecoration: "none"}}><span className="glyphicon glyphicon-book"></span> Repository</a></li>
             </ul>
           </div>
 
           <div className="jumbotron text-center">
             <div className="container">
-              <a href="/" className="lang-logo">
-                {/* <img src="http://localhost:3000/images/lang-logo.png"/> */}
-              </a>
-              <h1>Live Video Stream - Client</h1>
-              <p>Welcome to the Runestone Robot application! On this page you can see the video.</p>
+              <h1>GSDP Runestone Robot - Team 01</h1>
+              <p>Welcome to the Runestone Robot application! Follow the adventures of the robot as it navigates through the warehouse and moves packages between different locations!</p>
             </div>
           </div>
 
@@ -62,12 +59,12 @@ class App extends Component {
               <h1 className="w3-margin w3-xlarge">Team members:</h1>
               <h1 className="w3-margin w3-xlarge">Billy Clerc, Công Thành Bùi, Haubir Mariwani, Huy Pham, Krishnamurthy Gajapathy, Tu Pham</h1>
           </div>
-            
+
           <footer className="w3-container w3-padding-64 w3-center w3-opacity">
           <p>Original template provided by <a href="https://www.heroku.com/" target="_blank">Heroku</a> and <a href="https://www.w3schools.com/" target="_blank">w3schools.com</a></p>
           <p>Livestream over server credit to https://github.com/alexcambose/webcam-base64-streaming.git</p>
           </footer>
-          
+
       </div>
     );
   }
